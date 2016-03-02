d4ee309a76bd0e0b319ece2e12553d50e5808fdd
patrocc6 <patrocc6@gmail.com>
2016-03-02 20:29:48 +0000

    nitrous-backup at Wed Mar  2 20:29:48 UTC 2016

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,30 @@
 # Enter your procedural solution here!
+include Math
+
+def largest_prime_factor(number)
+  return number if prime?(number)
+
+ ( (2..sqrt(number).round).reverse_each do |num|
+    if number%num == 0 && prime?(num)
+      return num
+    end
+  end)
+
+  return nil
+end
+
+def prime?(number)
+  if number <= 0
+    return false
+  end
+
+  (2..sqrt(number).floor).reverse_each do |num|
+    if number%num == 0
+      return false
+    end
+  end
+
+  return true
+
+end
+
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,36 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+    include Math
+
+    def initialize(input)
+        @input = input
+    end
+
+    def number
+        return @input if prime?(@input)
+
+        (2..sqrt(@input).round).reverse_each do |num|
+            if @input%num == 0 && prime?(num)
+                return num
+            end
+        end
+
+        return nil
+    end
+
+    def prime?(number)
+        if number <= 0
+            return false
+        end
+
+        (2..sqrt(number).floor).reverse_each do |num|
+            if number%num == 0
+                return false
+            end
+        end
+
+        return true
+    end
+    
+end
+
